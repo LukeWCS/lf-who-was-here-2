b4d5b06e9ac66ebd5cbfcc00059b2ae9ab15a748
bb3mobi <anvar.all@mail.ru>
2015-02-18 20:28:15 +0600

    Create acp_wwh_info.php

---
+++ acp/acp_wwh_info.php
@@ -0,0 +1,36 @@
+<?php
+/**
+*
+* @package - NV "who was here?"
+* @copyright (c) nickvergessen - http://www.flying-bits.org/
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+/**
+* @ignore
+*/
+namespace bb3mobi\washere\acp;
+/**
+* @package module_install
+*/
+class acp_wwh_info
+{
+	function module()
+	{		
+		return array(
+			'filename'	=> '\bb3mobi\washere\acp\acp_wwh',
+			'title'		=> 'WWH_TITLE',
+			'version'	=> '1.0.0',
+			'modes'		=> array(
+				'config_wwh'	=> array(
+				'title'		=> 'WWH_CONFIG',
+				'auth'		=> 'ext_bb3mobi/washere && acl_a_board',
+				'cat'		=> array('ACP_BOARD_CONFIGURATION'),
+				),
+			),
+		);
+	}
+}
+
+?>
