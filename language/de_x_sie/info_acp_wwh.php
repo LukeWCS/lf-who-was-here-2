5b118b262fa008ff4afa8117f89e4c44c93bc96a
unknown <C:\Program Files\The Bat!\Mail>
2015-05-19 01:34:19 +0600

    Update language

--- language/de_x_sie/info_acp_wwh.php
+++ language/de_x_sie/info_acp_wwh.php
@@ -27,17 +27,17 @@ else if (!isset($phpbb_root_path))
 
 $lang = array_merge($lang, array(
 	'WWH_CONFIG'				=> 'Einstellungen',
-	'WWH_TITLE'					=> 'Wer War Da?',
+	'WWH_TITLE'				=> 'Wer War Da?',
 
 	'WWH_DISP_SET'				=> 'Anzeige Einstellungen',
 	'WWH_DISP_BOTS'				=> 'Zeige Bots',
 	'WWH_DISP_BOTS_EXP'			=> 'Einige Benutzer werden sich fragen was Bots sind und sie fürchten.',
 	'WWH_DISP_GUESTS'			=> 'Zeige Gäste',
-	'WWH_DISP_GUESTS_EXP'		=> 'Zeige die Anzahl der Gäste',
+	'WWH_DISP_GUESTS_EXP'			=> 'Zeige die Anzahl der Gäste',
 	'WWH_DISP_HIDDEN'			=> 'Zeige unsichtbare Benutzer',
-	'WWH_DISP_HIDDEN_EXP'		=> 'Zeige die Anzahl der unsichtbaren Benutzer (benötigt Berechtigung)',
+	'WWH_DISP_HIDDEN_EXP'			=> 'Zeige die Anzahl der unsichtbaren Benutzer (benötigt Berechtigung)',
 	'WWH_DISP_TIME'				=> 'Zeige Zeit',
-	'WWH_DISP_TIME_FORMAT'		=> 'Zeit-Format',
+	'WWH_DISP_TIME_FORMAT'			=> 'Zeit-Format',
 	'WWH_DISP_HOVER'			=> 'Anzeige bei überfahren',
 	'WWH_DISP_TIME_EXP'			=> 'Allen Benutzern zeigen oder niemandem. Keine speziellen Funktionen für Administratoren.',
 	'WWH_DISP_IP'				=> 'Zeige die Besucher-IP',
@@ -47,12 +47,12 @@ $lang = array_merge($lang, array(
 
 	'WWH_RECORD'				=> 'Besucherrekord',
 	'WWH_RECORD_EXP'			=> 'Zeige und speichere den Besucherrekord',
-	'WWH_RECORD_TIMESTAMP'		=> 'Datums-Format für den Besucherrekord',
-	'WWH_RESET'					=> 'Besucherrekord zurücksetzen',
+	'WWH_RECORD_TIMESTAMP'			=> 'Datums-Format für den Besucherrekord',
+	'WWH_RESET'				=> 'Besucherrekord zurücksetzen',
 	'WWH_RESET_EXP'				=> 'Setzt Zeit und Zähler für den Aktivitäten-Rekord zurück.',
-	'WWH_RESET_TRUE'			=> 'Wenn du diese Seite speicherst,\nwird der Rekord zurückgesetzt.',
+	'WWH_RESET_TRUE'			=> 'Wenn Sie diese Seite speichern,\nwird der Rekord zurückgesetzt.',
 
-	'WWH_SAVED_SETTINGS'		=> 'Wer War Da: Einstellungen gespeichert.',
+	'WWH_SAVED_SETTINGS'			=> 'Wer War Da: Einstellungen gespeichert.',
 	'WWH_SORT_BY'				=> 'Sortiere Benutzer nach',
 	'WWH_SORT_BY_EXP'			=> 'In welcher Reihenfolge sollen die Benutzer angezeigt werden?',
 	'WWH_SORT_BY_0'				=> 'Benutzername A -> Z',
@@ -62,22 +62,22 @@ $lang = array_merge($lang, array(
 	'WWH_SORT_BY_4'				=> 'Benutzer-ID aufsteigend',
 	'WWH_SORT_BY_5'				=> 'Benutzer-ID absteigend',
 
-	'WWH_UPDATE_NEED'			=> 'Aktualisiere die "Wer War Da?" Erweiterung. Führe dazu die <a style="font-weight: bold;" href="' . $phpbb_root_path . 'install/index.php">install/index.php</a> aus.<br />Wenn du das getan hast, solltest du das install/ Verzeichnis wieder entfernen.',
+	'WWH_UPDATE_NEED'			=> 'Aktualisieren Sie die "Wer War Da?" Erweiterung. Führen Sie dazu die <a style="font-weight: bold;" href="' . $phpbb_root_path . 'install/index.php">install/index.php</a> aus.<br />Wenn Sie das getan haben, sollten Sie das install/ Verzeichnis wieder entfernen.',
 
 	'WWH_VERSION'				=> 'Anzeige der Besucher von ...',
 	'WWH_VERSION_EXP'			=> 'Anzeige der Besucher von heute (Seit 00:00 Forum Zeitzone), oder des Zeitraums der bei der nächsten Einstellung festgelegt wird.',
 	'WWH_VERSION1'				=> 'Heute',
 	'WWH_VERSION2'				=> 'Zeitraum',
 	'WWH_VERSION2_EXP'			=> '0 eingeben wenn die Besucher der letzten 24 Stunden angezeigt werden sollen',
-	'WWH_VERSION2_EXP2'			=> 'deaktiviert, wenn du "Heute" gewählt hast',
+	'WWH_VERSION2_EXP2'			=> 'deaktiviert, wenn Sie "Heute" gewählt haben',
 	'WWH_VERSION2_EXP3'			=> 'Sekunden',
 
 
-	'WWH_MOD'					=> '"Who was here?" Erweiterung',
+	'WWH_MOD'				=> '"Who was here?" Erweiterung',
 	'INSTALL_WWH_MOD'			=> '"Who was here?" Erweiterung installieren',
-	'INSTALL_WWH_MOD_CONFIRM'	=> 'Bist du sicher, dass du die "Who was here?" Erweiterung installieren möchtest?',
+	'INSTALL_WWH_MOD_CONFIRM'		=> 'Sind Sie sicher, dass Sie die "Who was here?" Erweiterung installieren möchten?',
 	'UPDATE_WWH_MOD'			=> '"Who was here?" Erweiterung aktualisieren',
-	'UPDATE_WWH_MOD_CONFIRM'	=> 'Bist du sicher, dass du die "Who was here?" Erweiterung aktualisieren möchtest?',
+	'UPDATE_WWH_MOD_CONFIRM'		=> 'Sind Sie sicher, dass Sie die "Who was here?" Erweiterung aktualisieren möchten?',
 	'UNINSTALL_WWH_MOD'			=> '"Who was here?" Erweiterung deinstallieren',
-	'UNINSTALL_WWH_MOD_CONFIRM'	=> 'Bist du sicher, dass du die "Who was here?" Erweiterung deinstallieren möchtest?',
+	'UNINSTALL_WWH_MOD_CONFIRM'		=> 'Sind Sie sicher, dass Sie die "Who was here?" Erweiterung deinstallieren möchten?',
 ));
