191ac613a11ac07840c03e26fa47e8a49b508fde
LukeWCS <luke@wcsaga.org>
2018-10-18 11:56:13 +0200

    1-4-0-b4

--- bb3mobi/washere/migrations/v_1_4_0.php
+++ bb3mobi/washere/migrations/v_1_4_0.php
@@ -36,15 +36,22 @@ class v_1_4_0 extends \phpbb\db\migration\migration
 			array('config.add', array('wwh_api_mode', '0')),
 			array('config.add', array('wwh_use_permissions', '0')),
 			// Add permissions
-			array('permission.add', array('u_wwh_show_users')),
 			array('permission.add', array('u_wwh_show_stats')),
+			array('permission.add', array('u_wwh_show_users')),
+			// Set permissions
 			array('permission.permission_set', array('ADMINISTRATORS', 'u_wwh_show_users', 'group')),
-			array('permission.permission_set', array('ADMINISTRATORS', 'u_wwh_show_stats', 'group')),
 			array('permission.permission_set', array('GLOBAL_MODERATORS', 'u_wwh_show_users', 'group')),
-			array('permission.permission_set', array('GLOBAL_MODERATORS', 'u_wwh_show_stats', 'group')),
-			array('permission.permission_set', array('REGISTERED', 'u_wwh_show_users', 'group')),
 			array('permission.permission_set', array('REGISTERED', 'u_wwh_show_stats', 'group')),
+			array('permission.permission_set', array('REGISTERED', 'u_wwh_show_users', 'group')),
+			array('permission.permission_set', array('NEWLY_REGISTERED', 'u_wwh_show_users', 'group', false)),
 			array('permission.permission_set', array('GUESTS', 'u_wwh_show_stats', 'group')),
+			// Set permission roles
+			array('permission.permission_set', array('ROLE_USER_STANDARD', 'u_wwh_show_users', 'role')),
+			array('permission.permission_set', array('ROLE_USER_LIMITED', 'u_wwh_show_users', 'role')),
+			array('permission.permission_set', array('ROLE_USER_FULL', 'u_wwh_show_users', 'role')),
+			array('permission.permission_set', array('ROLE_USER_NOPM', 'u_wwh_show_users', 'role')),
+			array('permission.permission_set', array('ROLE_USER_NOAVATAR', 'u_wwh_show_users', 'role')),
+			array('permission.permission_set', array('ROLE_USER_NEW_MEMBER', 'u_wwh_show_users', 'role', false)),
 			// Set current version
 			array('config.update', array('wwh_mod_version', '1.4.0')),
 		);
