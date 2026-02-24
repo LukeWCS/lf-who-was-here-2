39a2f79a7a5827cd73e13fe48b5ae6a3fae70354
LukeWCS <luke@wcsaga.org>
2026-02-24 21:19:36 +0100

    2.3.0-b7

--- lukewcs/whowashere/styles/all/template/who_was_here.js
+++ lukewcs/whowashere/styles/all/template/who_was_here.js
@@ -24,7 +24,7 @@
 		$('.lfwwh_hidden_' + e.data.ButtonType).toggle(isShown[e.data.ButtonType]);
 		$('.lfwwh_button_' + e.data.ButtonType).css('opacity', (isShown[e.data.ButtonType] ? '1.0' : '0.5'));
 		if (e.data.ButtonType.slice(0, 4) == 'all_') {
-			$('.lfwwh_button_' + e.data.ButtonType + '_pre').toggle(!isShown[e.data.ButtonType]);
+			$('.lfwwh_prefix_' + e.data.ButtonType).toggle(!isShown[e.data.ButtonType]);
 		}
 	}
 
